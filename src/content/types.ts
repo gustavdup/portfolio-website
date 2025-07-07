0267a973cdd03cba571cfe9510c295bf5d6a9aa4
Gustav du Plessis <gustavdup90@gmail.com>
2025-07-07 15:05:41 +0200

    removed old config file and updated types.ts

--- src/content/types.ts
+++ src/content/types.ts
@@ -1,19 +1,40 @@
 import type { CollectionEntry } from "astro:content";
 
-export interface BlogPost extends CollectionEntry<"blog"> {}
+// Core content types
+export interface Article extends CollectionEntry<"articles"> {}
 export interface Project extends CollectionEntry<"projects"> {}
+export interface Bio extends CollectionEntry<"bio"> {}
+export interface Education extends CollectionEntry<"education"> {}
+export interface CurrentRole extends CollectionEntry<"currentRoles"> {}
+export interface Page extends CollectionEntry<"pages"> {}
+export interface WorkWithMe extends CollectionEntry<"workWithMe"> {}
 
-// Experience collection types
-export interface ExperienceProduct extends CollectionEntry<"experienceProduct"> {}
-export interface ExperienceTechnology extends CollectionEntry<"experienceTechnology"> {}
-export interface ExperienceStrategy extends CollectionEntry<"experienceStrategy"> {}
-export interface ExperienceUX extends CollectionEntry<"experienceUX"> {}
-export interface ExperienceLeadership extends CollectionEntry<"experienceLeadership"> {}
+// Experience collection types - new role-based structure
+export interface ExperienceProductStrategist extends CollectionEntry<"experienceProductStrategist"> {}
+export interface ExperienceExecutionLead extends CollectionEntry<"experienceExecutionLead"> {}
+export interface ExperienceDigitalEnabler extends CollectionEntry<"experienceDigitalEnabler"> {}
+export interface ExperienceFractionalPm extends CollectionEntry<"experienceFractionalPm"> {}
+export interface ExperienceCollaborativeLeader extends CollectionEntry<"experienceCollaborativeLeader"> {}
+export interface ExperienceCommercialStrategist extends CollectionEntry<"experienceCommercialStrategist"> {}
+export interface ExperienceStrategicTechnologist extends CollectionEntry<"experienceStrategicTechnologist"> {}
 
 // Union type for all experience types
 export type Experience = 
-  | ExperienceProduct 
-  | ExperienceTechnology 
-  | ExperienceStrategy 
-  | ExperienceUX 
-  | ExperienceLeadership;
+  | ExperienceProductStrategist
+  | ExperienceExecutionLead
+  | ExperienceDigitalEnabler
+  | ExperienceFractionalPm
+  | ExperienceCollaborativeLeader
+  | ExperienceCommercialStrategist
+  | ExperienceStrategicTechnologist;
+
+// Experience collections mapping for component props
+export interface ExperienceCollections {
+  experienceProductStrategistExperiences: ExperienceProductStrategist[];
+  experienceExecutionLeadExperiences: ExperienceExecutionLead[];
+  experienceDigitalEnablerExperiences: ExperienceDigitalEnabler[];
+  experienceFractionalPmExperiences: ExperienceFractionalPm[];
+  experienceCollaborativeLeaderExperiences: ExperienceCollaborativeLeader[];
+  experienceCommercialStrategistExperiences: ExperienceCommercialStrategist[];
+  experienceStrategicTechnologistExperiences: ExperienceStrategicTechnologist[];
+}
